0be031dae7317d65dc532ac331c4050c8e9ec276
Admin <Admin@DESKTOP-MAHVHFA>
2026-03-02 10:39:19 +0300

    class InfoEmailSending

--- UsersClasses/InfoEmailSending.cs
+++ UsersClasses/InfoEmailSending.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +9,28 @@ namespace LR5_s8.UsersClasses
 {
     internal class InfoEmailSending
     {
+        public InfoEmailSending(string smtpClientAdress,
+            StringPair emailAdressFrom,
+            string emailPassword,
+            StringPair emailAdressTo,
+            string subject,
+            string body)
+        {
+            SmtpClientAdress = String.IsNullOrWhiteSpace(smtpClientAdress) ?
+                 throw new Exception("Нельзя вставлять пробелы или пустые значения!") : smtpClientAdress;
+            EmailAdressFrom = emailAdressFrom ?? throw new ArgumentNullException(nameof(emailAdressFrom));
+            EmailPassword = String.IsNullOrWhiteSpace(emailPassword) ?
+                 throw new Exception("Нельзя вставлять пробелы или пустые значения!") : emailPassword;
+            EmailAdressTo = emailAdressFrom ?? throw new ArgumentNullException(nameof(emailAdressTo));
+            Subject = subject ?? throw new ArgumentNullException(nameof(subject));
+            Body = body ?? throw new ArgumentNullException(body);
+        }
+        public string SmtpClientAdress { get; set; }
+        public StringPair EmailAdressFrom { get; set; }
+        public string EmailPassword { get; set; }
+        public StringPair EmailAdressTo { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
         public class StringPair
         {
             public StringPair(string emailAdress, string name)
